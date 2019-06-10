e8f85f9257b4e6a7b0db92f9f915da85e94275cb
Reymai <32044058+Reymai@users.noreply.github.com>
2019-06-10 08:38:52 +0300

    Revert "Revert "i don't know what to write now""
    This reverts commit 94d239832476b199069d41b7fe222f4938f9aa02.

--- ProjectCity/map.h
+++ ProjectCity/map.h
@@ -1,16 +1,1 @@
 #pragma once
-#include <string>
-#include <SFML/Graphics.hpp>
-
-#include "tinyxml2.h"
-
-class Map : public sf::Drawable, sf::Transformable{
-private:
-	void draw(sf::RenderTarget& target, sf::RenderStates states);
-	
-public:
-	Map(const std::string& filename);
-
-	void update();
-};
-
---
+++ ProjectCity/media/road.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="road" tilewidth="109" tileheight="70" tilecount="20" columns="5">
+ <image source="road.svg" trans="ffffff" width="550" height="280"/>
+</tileset>
