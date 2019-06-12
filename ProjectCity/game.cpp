d731031be1f8552c958b9274720732950c23892d
Reymai <32044058+Reymai@users.noreply.github.com>
2019-06-12 15:57:22 +0300

    update

--- ProjectCity/game.cpp
+++ ProjectCity/game.cpp
@@ -7,7 +7,8 @@ int Game::load(sf::RenderWindow& window) {
 	// load font
 	if (!font.loadFromFile("media/font.ttf"))
 		return EXIT_FAILURE;
-	tileset.loadFromFile ("media/road.png");
+	if(!tileset.loadFromFile ("media/test.png"))
+		return EXIT_FAILURE;
 	Game::draw(window);
 }
 
@@ -17,12 +18,35 @@ int Game::draw(sf::RenderWindow& window) {
 
 	sf::View view;
 
-	Map map(tileset, 10, 10, 70.f, 140.f);
+	const int level[] =
+	{
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+	};
+
+	Map map;
+	if (!map.load ("media/road.png", sf::Vector2u (108, 70), level, 16, 8))
+		return -1;
 	/*tmx::TileMap map("media/map.tmx");*/
 
 	sf::Keyboard keyboard;
 
 	/*map.ShowObjects();*/
+
+	sf::ConvexShape convex;
+
+	convex.setPointCount (4);
+
+	convex.setPoint (0, sf::Vector2f (0, 26));
+	convex.setPoint (1, sf::Vector2f (52, 53.3f));
+	convex.setPoint (2, sf::Vector2f (108.5f, 26));
+	convex.setPoint (3, sf::Vector2f (52, 0));
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -34,10 +58,11 @@ int Game::draw(sf::RenderWindow& window) {
 		if (keyboard.isKeyPressed) {
 			movement(keyboard, view);
 		}
-		window.clear();
-		window.draw(background);
-		window.draw(map);
-		window.display();
+		window.clear ();
+		window.draw (background);
+		window.draw (map);
+		window.draw (convex);
+		window.display ();
 	}
 	return EXIT_SUCCESS;
 }
